4d86b2dc8bbed33db3e0932783ee1af0ca0944c5
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-12 09:59:54 -0600

    Programa 1

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,28 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Larisa Carolina Alvarez Gonzalez 763374
 """
 
-# Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+numero1 = int(input("Introduzca un número: "))
+numero2 = int(input("Introduzca otro número: "))
+
 
 # Proceso
+numero = 0
+multiplo = 0
+
+if numero1 % numero2 == 0:
+    multiplo = numero1
+    numero = numero2
+elif numero2 % numero1 == 0:
+    multiplo = numero2
+    numero = numero1
 
 
 # Salidas
-print(salida)
+print("El número " + str(multiplo) + " es múltiplo del " + str(numero))
+
+
+
