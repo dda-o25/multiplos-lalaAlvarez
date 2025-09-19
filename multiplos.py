c2c91a693217453ce746da1a96ffc3b86b633249
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-19 07:38:02 -0600

    cuando hay 0

--- multiplos.py
+++ multiplos.py
@@ -12,17 +12,24 @@ numero2 = int(input("Introduzca otro número: "))
 # Proceso
 numero = 0
 multiplo = 0
+salida = ""
+if numero1 == 0 or numero2 == 0:
+    salida="Ninguno de los numeros es múltiplo del otro"    
+else:
+    if numero1 % numero2 == 0:
+        multiplo = numero1
+        numero = numero2
+        salida = "El número " + str(multiplo) + " es múltiplo del " + str(numero)
+    elif numero2 % numero1 == 0:
+        multiplo = numero2
+        numero = numero1
+        salida = "El número " + str(multiplo) + " es múltiplo del " + str(numero)
+    else:
+        salida="Ninguno de los numeros es múltiplo del otro"    
 
-if numero1 % numero2 == 0:
-    multiplo = numero1
-    numero = numero2
-elif numero2 % numero1 == 0:
-    multiplo = numero2
-    numero = numero1
-    
 
 # Salidas
-print("El número " + str(multiplo) + " es múltiplo del " + str(numero))
+print(salida)
 
 
 
