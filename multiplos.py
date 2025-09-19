f978deaefe0fd7592510052ecfdd0c40c825a715
lalaAlvarez <larisa.alvarez@iteso.mx>
2025-09-19 08:19:08 -0600

    division entre 0 correcion

--- multiplos.py
+++ multiplos.py
@@ -14,13 +14,23 @@ numero = 0
 multiplo = 0
 salida = ""
 if numero1 == 0 and numero2 == 0:
-    salida="Ninguno de los números es múltiplo del otro"    
+    salida="Ninguno de los números es múltiplo del otro"   
+elif numero1 == 0:
+    multiplo = numero1
+    numero = numero2
+    salida = "El número " + str(multiplo) + " es múltiplo del " + str(numero)
+
+elif numero2 == 0:
+    multiplo = numero2
+    numero = numero1
+    salida = "El número " + str(multiplo) + " es múltiplo del " + str(numero)
+
 else:
-    if numero1 % numero2 == 0 and numero2 != 0:
+    if numero1 % numero2 == 0 :
         multiplo = numero1
         numero = numero2
         salida = "El número " + str(multiplo) + " es múltiplo del " + str(numero)
-    elif numero2 % numero1 == 0 and numero1 != 0:
+    elif numero2 % numero1 == 0:
         multiplo = numero2
         numero = numero1
         salida = "El número " + str(multiplo) + " es múltiplo del " + str(numero)
